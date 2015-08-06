244fd6a24cd2e7a77adac6d45ba2c2fff3bc3cb1
noite <noite@SPEED2010-17.aluno.local>
2015-08-05 21:03:09 -0300

    inicializando GitHup projeto
    trabalhandoi com classes

---
+++ src/introjava/Animal.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author noite
+ */
+public class Animal {
+    String nome;
+    int idade;
+    int peso;
+    String CorDosOlhos;
+    
+    public int Comer(){
+    
+    return 1;
+    }
+    
+    public int Dormir(){
+    
+    return 1;
+    }
+    
+    
+}
---
+++ src/introjava/Aves.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package introjava;
+
+/**
+ *
+ * @author noite
+ */
+public class Aves {
+    String CorDasPenas = null;
+    int TamanhoDasPernas;
+    
+    public int BotarOvo(){
+    
+    return 1;
+    }
+}
