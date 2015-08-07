aa349b7983602d14718af994523e05bf796adfd4
noite <noite@SPEED2010-17.aluno.local>
2015-08-06 22:49:03 -0300

    inicializando GitHup projeto
    trabalhandoi com classes

--- src/introjava/IntroJava.java
+++ src/introjava/IntroJava.java
@@ -6,6 +6,7 @@
 package introjava;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 /**
  *
@@ -13,43 +14,45 @@ import java.util.Scanner;
  */
 public class IntroJava {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public  enum showmenu {
+    
+    SALVAR(1),IMPRIMIR(2),ABRIR(3),VISUALIZAR(4),FECHAR(5);
+    
+    private final int valor;
+    showmenu(int valorOpcao){
+    valor = valorOpcao;
+     }
+    public int getValor(){
+     return valor;
+    }
+    
+    }
+    public static void main(String[] args) {        
+         escolhe(showmenu.ABRIR); 
         
+        Scanner teste = new Scanner(System.in);
+        System.out.println("informe um valor");
+        int retorno = teste.nextInt();
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//      AULA DE QUARTA-FAIRA    
-        
-//        Scanner teste = new Scanner(System.in);
-//        System.out.println("informe um valor");
-//        int retorno = teste.nextInt();
-//        
-//        while (retorno !=0) {
-//            System.out.println("informe outro  valor");
-//            retorno = teste.nextInt();                  
-//        }
-//        System.out.println("informado com sucesso");
+        while (retorno !=0) {
+            System.out.println("informe outro  valor");
+            retorno = teste.nextInt();                  
+        }
+        System.out.println("informado com sucesso");
         
     }
     
+    public static void escolhe(showmenu Opcao){
+    
+        if(Opcao == showmenu.SALVAR){
+            System.out.println("salvando");
+            
+        
+        }else if (Opcao == showmenu.ABRIR){
+              System.out.println("Abrindo arquivos");
+        
+        
+        }
+    
+}
 }
